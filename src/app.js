73c11183f00e66f3556a7a0b0b95d0b2b459e338
YamilaBog <yambogarin@gmail.com>
2022-07-28 17:24:44 -0300

    errores de views

--- src/app.js
+++ src/app.js
@@ -5,5 +5,5 @@ app.use(express.static("./public"));
 app.set ('view engine','ejs');
 app.use('/',mainRoutes);
 app.listen(3000, () =>{
-     console.log("Servidor Funcionando")
+     console.log("Servidor Funcionando en 3000")
 });
