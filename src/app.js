1377fa92dc74cb93195b25309d7890d77e9ec33e
FedericoFores <fedefores1234567@gmail.com>
2022-07-27 22:47:56 -0300

    Carpetas creadas para comenzar a trabajar

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
-console.log("Holaa")
-console.log("Chauuuu")
-console.log("jeje")
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const publicPath = path.resolve(__dirname, './public')
+app.use( express.static(publicPath) )
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
+})
+
+app.get('/vistaTurnos', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/vistaTurnos.html'))
+})
