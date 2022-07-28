b1b126e75ac1f966b517fa3c546cda2c9cc628b2
YamilaBog <yambogarin@gmail.com>
2022-07-27 23:59:46 -0300

    primer css y html

--- src/app.js
+++ src/app.js
@@ -1,20 +1,10 @@
-const express = require('express')
-const path = require('path')
-
+const express = require ("express")
+const path = require("path")
 const app = express()
+const PORT = process.env.PORT || 3000;
 
-const publicPath = path.resolve(__dirname, './public')
-app.use( express.static(publicPath) )
+app.use(express.static(path.join(__dirname,"../","public")))
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000')
-})
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")))
 
-app.get('/vistaTurnos', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/vistaTurnos.html'))
-})
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'))
-})
-
+app.listen(PORT, () => console.log("Servidor Funcionando"))
