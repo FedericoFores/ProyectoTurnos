ee3ba3b826129bdd74b4e7e41b9b998f6254c5ab
FedericoFores <fedefores1234567@gmail.com>
2022-07-28 23:04:22 -0300

    Vista listadoDeTurnos

--- src/app.js
+++ src/app.js
@@ -6,5 +6,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname,"../","public")))
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "vistaTurnos.html")))
+app.get("/opcionesDeTurnos", (req, res) => res.sendFile(path.join(__dirname, "views", "listadoDeTurnos.html")))
 
 app.listen(PORT, () => console.log("Servidor Funcionando"))
