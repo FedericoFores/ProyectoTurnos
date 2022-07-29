6ae448a3262eb354eefdc934a3608d4c2cec56e2
YamilaBog <yambogarin@gmail.com>
2022-07-29 01:10:22 -0300

    Correccion del error de no encuentra directorio de log

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require ("path");
 const controlador = {
     index:(req,res) => {res.render("vistaTurnos.ejs")},
     login:(req,res) => {res.render("login")},
