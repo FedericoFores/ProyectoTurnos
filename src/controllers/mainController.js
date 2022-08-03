62a03aabb1d11d0ab3ade704492d431038fa93f7
YamilaBog <yambogarin@gmail.com>
2022-08-02 23:30:16 -0300

    diseño de form

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,6 +3,7 @@ const controlador = {
     index:(req,res) => {res.render("vistaTurnos.ejs")},
     login:(req,res) => {res.render("login")},
     listadoDeTurnos:(req,res) => {res.render("listadoDeTurnos")},
+    turno:(req,res) => {res.render("turno")},
 }
 
 module.exports = controlador;
