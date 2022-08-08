adb99ee3bf7cfcb8e2886e99eac2077e0eb1129d
FedericoFores <fedefores1234567@gmail.com>
2022-08-07 23:12:48 -0300

    Errores sequalize

--- src/DataBase/turno.sql
+++ src/DataBase/turno.sql
@@ -1,34 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 05, 2022 at 03:39 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `sistema turnos`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orderly_turn`
---
-
-CREATE Database if not exists "Sistema Turnos"
-use "Sistema Turnos";
+CREATE Database if not exists `sistemaTurnos`;
+use `sistemaTurnos`;
 
 CREATE TABLE `orderly_turn` (
   `id` smallint(6) UNSIGNED AUTO_INCREMENT NOT NULL,
@@ -36,7 +7,8 @@ CREATE TABLE `orderly_turn` (
   `box` text NOT NULL,
   `created_at` date NOT NULL,
   `updated_at` date NOT NULL,
-  `deleted_at` date NOT NULL
+  `deleted_at` date NOT NULL,
+   	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -51,10 +23,6 @@ CREATE TABLE `users` (
   `email` text NOT NULL,
   `password` text NOT NULL,
   `created_at` date NOT NULL,
-  `updated_at` date NOT NULL
+  `updated_at` date NOT NULL,
+   	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,18 @@
+const baseDeDatos = require("../database/models")
 const path = require ("path");
 const controlador = {
     index:(req,res) => {res.render("vistaTurnos.ejs")},
     login:(req,res) => {res.render("login")},
     listadoDeTurnos:(req,res) => {res.render("listadoDeTurnos")},
-    turno:(req,res) => {res.render("turno")},
+    turno:async(req,res) => {
+    const letra = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    let letraAleatoria = letra[Math.floor(Math.random() * 27)];
+    let valor = Math.floor(Math.random() * 99)
+    let turno = `${letraAleatoria}${valor}`
+    await baseDeDatos.orderly_turn.create({
+        code:turno, box:""
+    })
+    return turno}
 }
 
 module.exports = controlador;
