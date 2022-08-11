25c9501fc2a11e5b25496f3e3ab83364ac5bdd86
YamilaBog <yambogarin@gmail.com>
2022-08-11 18:03:48 -0300

    rompiendo modelo original para ver perfiles de usuario

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require ("express")
 const app = express()
 const path = require ("path");
 const mainRoutes = require ("./routes/mainRoutes");
+const validator = require(path.join(__dirname, '..', 'middleware', 'express-validator.js'));
+const guestMiddleware = require("../middleware/guestMiddleware");
+const { body } = require('express-validator');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,10 +1,22 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const Router = express.Router();
 const mainController = require("../controllers/mainController")
-const router = express.Router();
-const path = require("path");
+const validator = require(path.join(__dirname, '..', 'middleware', 'express-validator.js'));
+const guestMiddleware = require("../middleware/guestMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
+const { body } = require('express-validator');
+
+router.get('/login', guestMiddleware, mainController.login);
+router.post('/login', [
+    body('email')
+        .isEmail().withMessage('Por favor escribe un email válido'),
+    body('password')
+        .notEmpty().withMessage('Por favor escribe tu contraseña').bail()
+        .isLength({ min: 8 }).withMessage('Su contraseña debe tener más de ocho caracteres.')
+], userController.processLogin);
 
 router.get('/',mainController.index);
-router.get('/login',mainController.login);
 router.get('/listadoDeTurnos',mainController.listadoDeTurnos);
 router.get('/admin/turno',mainController.turno);
 router.get('/admin/sector',mainController.sector);
@@ -14,6 +26,15 @@ router.post('/login',mainController.login);
 router.get ("/add",mainController.add);
 router.post ("/add",mainController.create);
 
-/*router.post('/login',userController.fuctionloLogin);*/
+router.get("/profile", authMiddleware, mainController.profile);
+router.get('/logout', mainController.logout);
 
-module.exports = router
+module.exports = router;
+
+
+
+
+
+
+
+
