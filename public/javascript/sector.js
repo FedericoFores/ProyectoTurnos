98fa9af696800dc6be6b59e782076138701bc2b9
YamilaBog <yambogarin@gmail.com>
2022-08-11 21:15:46 -0300

    corrigiendo rutas - version final

--- public/javascript/sector.js
+++
@@ -1,4 +0,0 @@
-const botonTurnos = document.querySelector("#botonGenerarTurno")
-botonTurnos.addEventListener("click", () => {
-    fetch("http://localhost:3000/admin/sector").then(respuesta => respuesta.json()).then(respuestaf => console.log(respuestaf))
-})
